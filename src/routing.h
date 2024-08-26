2beb2d5297594371a53a35b05cdf5e2491239c97
Jörn Fischbach <93682131+InfiniteLevel@users.noreply.github.com>
2024-08-26 16:29:10 +0200

    Revert "Merge branch 'Fischbach'"
    This reverts commit c7f7bd4f9f5eb2683f4d696d580b572e15c208a3

--- src/routing.h
+++ src/routing.h
@@ -6,22 +6,13 @@
 #include <list>
 #include <random>
 #include <set>
-#include <set>
 #include <vector>
 
-#include "dht_server.h"
-#include "common_types.h"
 #include "dht_server.h"
 #include "common_types.h"
 
 extern const size_t K;
 
-class KBucket {
-   private:
-    NodeID start;
-    NodeID end;
-    std::list<Node> peers;
-    std::list<Node> replacement_cache;
 class KBucket {
    private:
     NodeID start;
@@ -43,43 +34,7 @@ class KBucket {
 
 NodeID generate_random_nodeID();
 
-NodeID generate_random_nodeID();
-
 class RoutingTable {
-   private:
-    std::vector<KBucket> bucket_list;
-    Node local_node;
-
-   public:
-    void add_peer(const Node& peer);
-    void split_bucket();
-
-    size_t get_bucket_for(NodeID key);
-
-    std::vector<Node> find_closest_nodes(NodeID node_id);
-
-    RoutingTable(const in6_addr& ip, const in_port_t& port, const NodeID& id = generate_random_nodeID());
-    int get_shared_prefix_bits(KBucket bucket);
-    void split_bucket(KBucket bucket, int depth);
-
-    RoutingTable() = default;
-    const Node& get_local_node() const;
-    const std::vector<KBucket>& get_bucket_list() const;
-    static NodeID node_distance(const NodeID& node_1, const NodeID& node_2);
-    template<typename Iterable>
-    static bool has_duplicate_id(const Iterable& nodes);
-};
-
-template<typename Iterable>
-bool RoutingTable::has_duplicate_id(const Iterable& nodes) {
-    std::set<NodeID> seen_ids;
-    for (Node node : nodes) {
-        if (!seen_ids.insert(node.id).second) {
-            return true;
-        }
-    }
-    return false;
-}
    private:
     std::vector<KBucket> bucket_list;
     Node local_node;
